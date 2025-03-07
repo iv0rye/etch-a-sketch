94cce270582fd73971059ecea0ee2eacfde40cf1
Ivan Li <ivanli10252006@gmail.com>
2025-03-07 19:16:39 +1100

    added basic js script to generate grid
    note that css for vertical grid is messed up somehow as it isn't stretching the grids out.

---
+++ script.js
@@ -0,0 +1,10 @@
+for (let i = 0; i < 16; i++){
+    const verticalgrid = document.createElement('div');
+    verticalgrid.classList.add("verticalgrid");
+    outergrid.appendChild(verticalgrid);
+    for (let i=0; i < 16; i++){
+        const horizontalgrid = document.createElement('div');
+        horizontalgrid.classList.add("horizontalgrid");
+        verticalgrid.appendChild(horizontalgrid);
+    }
+}
