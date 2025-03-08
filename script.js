62c57efee57ac57b794a6aa05e85f8c44af06973
Ivan Li <ivanli10252006@gmail.com>
2025-03-09 01:03:27 +1100

    added drawing by hovering over grid

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-for (let i = 0; i < 16; i++){
+const gridSize = 64
+
+
+
+for (let i = 0; i < gridSize; i++){
     const verticalgrid = document.createElement('div');
     verticalgrid.classList.add("verticalgrid");
     outergrid.appendChild(verticalgrid);
-    for (let i = 0; i < 15; i++){
+    for (let i = 0; i < (gridSize - 1); i++){
         const horizontalgrid = document.createElement('div');
         horizontalgrid.classList.add("horizontalgrid");
         verticalgrid.appendChild(horizontalgrid);
+        horizontalgrid.addEventListener("mouseover", (e) => {
+            horizontalgrid.classList.add("hovered");
+        });
     }
 }
+
